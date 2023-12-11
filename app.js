e8d0287d3b153ceb0bf84e1af8b93b10e42382e4
Sapeksh Pareek <sapekshpareek136@gmail.com>
2023-12-11 20:05:14 +0530

    Adding Database

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main()
@@ -17,6 +18,18 @@ async function main() {
 
 app.get("/", (req, res) => {
   res.send("Hi, I am Root");
+});
+
+app.get("/testListing", async (req, res) => {
+  let sampleListing = new Listing({
+    title: "My Villa",
+    description: "By the Beach",
+    price: 1200,
+    location: "Calangute, Goa",
+  });
+  await sampleListing.save();
+  console.log("Sample Saved!");
+  res.send("successful Testing");
 });
 
 app.listen(8080, () => {
--- models/listing.js
+++ models/listing.js
@@ -9,12 +9,19 @@ const listingSchema = new Schema({
   description: String,
   image: {
     type: String,
-    set: (v) => v === "" ? "default link" : v,
+    default: "https://images.unsplash.com/photo-1489644484856-f3ddc0adc923?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1489644484856-f3ddc0adc923?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        : v,
   },
   price: Number,
   location: String,
-  country: String,
+  country: {
+    type: String,
+    default: "India",
+  },
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
-mongoose.export = Listing;
+module.exports = Listing;
