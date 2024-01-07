f0e225082773b2385267f7b5184969195a8ac407
Sapeksh Pareek <sapekshpareek136@gmail.com>
2024-01-07 17:02:30 +0530

    Owner Display

--- routes/listing.js
+++ routes/listing.js
@@ -54,6 +54,7 @@ router.post(
   validateListing,
   wrapAsync(async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
     await newListing.save();
     req.flash("success", "New Listing Created");
     res.redirect("/listings");
