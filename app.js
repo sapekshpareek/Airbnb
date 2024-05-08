a5f0db78475195a3c343fa91c3b7b6daa80e8447
Sapeksh Pareek <sapekshpareek136@gmail.com>
2024-05-08 11:13:50 +0530

    Background Navbar

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-// const dbURL = "mongodb://127.0.0.1:27017/wanderly";
-const dbURL = process.env.ATLASDB_URL;
+const dbURL = "mongodb://127.0.0.1:27017/wanderly";
+// const dbURL = process.env.ATLASDB_URL;
 
 main()
   .then(() => {
